e86f51d26954d92a5bc15387203913634f6eaf56
ningia <113545072+ningia92@users.noreply.github.com>
2025-04-29 18:43:27 +0200

    feat: create a new function that takes an operator and two numbers and calls one of the basic operations

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-const add = (num1, num2) => {
-  return num1 + num2
-}
+const add = (num1, num2) => num1 + num2
 
-const subtract = (num1, num2) => {
-  return num1 - num2
-}
+const subtract = (num1, num2) => num1 - num2
 
-const multiply  = (num1, num2) => {
-  return num1 * num2
-} 
+const multiply = (num1, num2) => num1 * num2
 
-const divide = (num1, num2) => {
-  return num1 / num2
-}
+const divide = (num1, num2) => num1 / num2
 
-let num1
-let num2
-let operator
+let num1 = 2
+let num2 = 3
+let operator = add
+
+const operate = (operator, num1, num2) => operator(num1, num2)
